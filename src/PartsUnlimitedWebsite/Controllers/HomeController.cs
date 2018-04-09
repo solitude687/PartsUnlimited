93e382a6288c24dec1b4da88beb63898b96fe40d
Chinonso Obiora <chinonso.obiora@gmail.com>
2018-04-09 12:12:29 +0000

    Updated HomeController.cs

--- src/PartsUnlimitedWebsite/Controllers/HomeController.cs
+++ src/PartsUnlimitedWebsite/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using PartsUnlimited.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+//CI trigger
 namespace PartsUnlimited.Controllers
 {
     public class HomeController : Controller
